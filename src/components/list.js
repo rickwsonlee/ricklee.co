3bc99053320964de3c4ca24f0cec5930d7ca1b05
Rick Lee <29583892+rickwsonlee@users.noreply.github.com>
2021-11-05 17:25:39 +0800

    Add gap prop to List component

--- src/components/list.js
+++ src/components/list.js
@@ -4,4 +4,8 @@ export const List = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
+
+  li:not(:last-child) {
+    margin-bottom: ${props => (props.gap ? `${props.gap}` : null)};
+  }
 `

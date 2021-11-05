864d1a46f954ca8c769d17aa75dc58e3890c8b4f
Rick Lee <29583892+rickwsonlee@users.noreply.github.com>
2021-11-05 15:31:41 +0800

    Create Header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,28 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
+import { Grid, Col } from "../components/grid"
+import { List } from "./list"
+
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
+  <header id="top">
+    <Grid sm="2">
+      <Col>
         <Link to="/">{siteTitle}</Link>
-      </h1>
-    </div>
+      </Col>
+      <Col>
+        <nav>
+          <List>
+            <li>
+              <a href="mailto:hello@ricklee.co">Email</a>
+            </li>
+            <li>
+              <a href="//www.linkedin.com/in/rickwsonlee">LinkedIn</a>
+            </li>
+          </List>
+        </nav>
+      </Col>
+    </Grid>
   </header>
 )
 

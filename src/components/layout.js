646eac97ee76c92fdd38e0292ec2087f7cb1e940
Rick Lee <29583892+rickwsonlee@users.noreply.github.com>
2021-11-08 12:25:50 +0800

    Customize title template for Index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,22 +26,12 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = ({ children, title }) => {
   return (
     <>
       <ThemeProvider theme={darkTheme}>
         <GlobalStyle />
-        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+        <Header siteTitle={title} />
         <main>{children}</main>
         <Footer />
       </ThemeProvider>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,33 @@
 import * as React from "react"
+import { graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = ({ data }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const siteDescription = data.site.siteMetadata?.description || `Description`
+
+  return (
+    <Layout title={siteTitle}>
     <Seo title="Home" />
+      <Helmet>
+        <title>{`${siteTitle} — ${siteDescription}`}</title>
+      </Helmet>
   </Layout>
 )
+}
 
 export default IndexPage
+
+export const siteQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`

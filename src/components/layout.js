6f46c1ba9da7661490d99a71efc3f5ce3d0f43f4
Rick Lee <29583892+rickwsonlee@users.noreply.github.com>
2021-12-20 11:19:22 +0800

    Add MDX provider

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { MDXProvider } from "@mdx-js/react"
 
 import fontURL from "@fontsource/archivo/files/archivo-latin-400-normal.woff2"
 
@@ -10,6 +11,8 @@ import Footer from "./footer"
 
 import { darkTheme } from "../theme"
 import { Reset } from "../reset"
+import { Section, Grid, Col } from "./grid"
+import { AnimatedAnchor } from "./link"
 
 const GlobalStyle = createGlobalStyle`
   // reset.js
@@ -22,6 +25,8 @@ const GlobalStyle = createGlobalStyle`
     background: ${({ theme }) => theme.body};
   }
 `
+
+const shortcodes = { Section, Grid, Col, AnimatedAnchor }
 
 const Layout = ({ children }) => {
   return (
@@ -38,7 +43,9 @@ const Layout = ({ children }) => {
       <ThemeProvider theme={darkTheme}>
         <GlobalStyle />
         <Header />
-        <main>{children}</main>
+        <main>
+          <MDXProvider components={shortcodes}>{children}</MDXProvider>
+        </main>
         <Footer />
       </ThemeProvider>
     </>

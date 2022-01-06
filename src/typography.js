6be80f83e67f088d9ca357b80cd83c9c7073b2bf
Rick Lee <29583892+rickwsonlee@users.noreply.github.com>
2022-01-06 15:35:19 +0800

    Add new styles

--- src/typography.js
+++ src/typography.js
@@ -4,8 +4,20 @@ export const Typography = css`
   h1 {
     font-size: 3rem;
   }
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  h3 {
+    font-size: 1.25rem;
+  }
 `
 
 export const Subhead = styled.p`
   font-size: 1.3rem;
+`
+
+export const Label = styled.p`
+  opacity: 0.5;
 `

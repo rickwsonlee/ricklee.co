997a5dcc583f8e28268921e747864bceb4bab6cc
Rick Lee <29583892+rickwsonlee@users.noreply.github.com>
2022-01-30 12:29:21 +0800

    Update list styles

--- src/components/list.js
+++ src/components/list.js
@@ -4,9 +4,10 @@ export const List = styled.ul`
   width: 100%;
   list-style: ${props => (props.nostyle ? `none` : null)};
   margin: 0;
+  margin-bottom: 1em;
   padding-left: ${props => (props.nostyle ? `0` : `2rem`)};
 
   li:not(:last-child) {
-    margin-bottom: ${props => (props.gap ? `${props.gap}` : null)};
+    margin-bottom: ${props => (props.gap ? `${props.gap}` : "8px")};
   }
 `

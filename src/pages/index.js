7d17bf98eaaae547e2a5950e04e65af54b556873
Rick Lee <29583892+rickwsonlee@users.noreply.github.com>
2022-01-30 12:52:20 +0800

    Update Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,32 +9,11 @@ import PostCard from "../components/post-card"
 import { Section, Grid, Col } from "../components/grid"
 import { List } from "../components/list"
 import { AnimatedAnchor } from "../components/link"
-import { HiddenText, Label } from "../typography"
+import { HiddenText, Display } from "../typography"
 
 const IndexPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const siteDescription = data.site.siteMetadata?.description || `Description`
-
-  const specialisations = [
-    `UI/UX`,
-    `Interaction Design`,
-    `Website Design`,
-    `Front-End Development`,
-  ]
-  const skills = [
-    `User Research`,
-    `Information Architecture`,
-    `Wireframing`,
-    `Prototyping`,
-    `Usability Testing`,
-    `Visual Design`,
-    `Design Systems`,
-    `HTML`,
-    `CSS`,
-    `JavaScript`,
-    `Gatsby (React)`,
-  ]
-  const posts = data.allMdx.nodes
 
   return (
     <Layout location={location}>
@@ -42,136 +21,92 @@ const IndexPage = ({ data, location }) => {
       <Helmet titleTemplate="">
         <title>{`${siteTitle} — ${siteDescription}`}</title>
       </Helmet>
-      <Section p="5rem 2rem">
-        <Grid lg="2" gap="2rem">
+      <Section p="5rem 3rem">
+        <Grid lg="2" gap="2rem 3rem">
           <Col>
             <HiddenText as="h2">Info</HiddenText>
-            <Grid lg="2" gap="2rem" sticky>
-              <Col gap="2rem">
-                <Section>
-                  <Label as="h3" transparent>
-                    Specialisation
-                  </Label>
-                  <List nostyle>
-                    {specialisations.map(specialisation => {
-                      return (
-                        <li key={specialisation.toString()}>
-                          {specialisation}
-                        </li>
-                      )
-                    })}
-                  </List>
-                </Section>
-                <Section>
-                  <Label as="h3" transparent>
-                    Skills
-                  </Label>
-                  <List nostyle>
-                    {skills.map(skill => {
-                      return <li key={skill.toString()}>{skill}</li>
-                    })}
-                  </List>
-                </Section>
-              </Col>
-              <Col gap="2rem">
-                <Section>
-                  <Label as="h3" transparent>
-                    Experience
-                  </Label>
-                  <List nostyle gap="1rem">
-                    <li>
-                      <dl>
-                        <dt>Web Designer</dt>
-                        <dd>Freelance</dd>
-                        <dd>2019 &ndash; Present</dd>
-                      </dl>
-                    </li>
-                    <li>
-                      <dl>
-                        <dt>Community Team</dt>
-                        <dd>Common Ground Coworking Space</dd>
-                        <dd>2019</dd>
-                      </dl>
-                    </li>
-                  </List>
-                </Section>
-                <Section>
-                  <Label as="h3" transparent>
-                    Education
-                  </Label>
-                  <List nostyle gap="1rem">
-                    <li>
-                      <dl>
-                        <dt>Google UX Design Professional Certificate</dt>
-                        <dd>Current</dd>
-                      </dl>
-                    </li>
-                    <li>
-                      <dl>
-                        <dt>Graduated in Engineering</dt>
-                        <dd>2015 &ndash; 2017</dd>
-                      </dl>
-                    </li>
-                  </List>
-                </Section>
-                <Section>
-                  <Label as="h3" transparent>
-                    Contact
-                  </Label>
-                  <List nostyle gap="1rem">
-                    <li>
-                      <AnimatedAnchor href="mailto:hello@ricklee.co" underlined>
-                        <span>hello&#64;ricklee.co</span>
-                      </AnimatedAnchor>
-                    </li>
-                    <li>
-                      <AnimatedAnchor
-                        href="//www.linkedin.com/in/rickwsonlee"
-                        target="_blank"
-                        rel="noopener"
-                        underlined
-                        external
-                      >
-                        <span>LinkedIn</span>
-                      </AnimatedAnchor>
-                    </li>
-                  </List>
-                </Section>
-              </Col>
+            <Grid lg="1" md="1" gap="4rem" sticky>
+              <div>
+                <Display>
+                  Hey. My name is Rick. I am a Product Designer from Penang,
+                  Malaysia, now based in Kuala Lumpur.
+                </Display>
+                <Display>
+                  I design interfaces and interactive experiences to delight and
+                  inspire.
+                </Display>
+                <Display>
+                  My work encompasses UI/UX design, web/mobile app strategy
+                  &amp; design, and front-end web development. I am{" "}
+                  <AnimatedAnchor
+                    href="//www.linkedin.com/in/rickwsonlee"
+                    target="_blank"
+                    rel="noopener"
+                    underlined
+                  >
+                    <span>available for hire</span>
+                  </AnimatedAnchor>
+                  .
+                </Display>
+                <Display>
+                  I nerd out on random things on a daily basis but hey, gotta do
+                  the research.
+                </Display>
+              </div>
+              <List nostyle gap="4px">
+                <li>
+                  <AnimatedAnchor
+                    href="//1drv.ms/b/s!AjrODPFRPE3lgesDwA1paB6J727ksQ?e=PuhFro"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <Display as="span">Resume</Display>
+                  </AnimatedAnchor>
+                </li>
+                <li>
+                  <AnimatedAnchor
+                    href="//www.linkedin.com/in/rickwsonlee"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <Display as="span">LinkedIn</Display>
+                  </AnimatedAnchor>
+                </li>
+                <li>
+                  <AnimatedAnchor
+                    href="//github.com/rickwsonlee"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <Display as="span">GitHub</Display>
+                  </AnimatedAnchor>
+                </li>
+              </List>
             </Grid>
           </Col>
-          <Col>
-            <HiddenText as="h2" id="archive">
-              Archive
-            </HiddenText>
-            <List nostyle gap="2rem">
-              {posts.map(post => {
-                return <PostCard post={post} />
-              })}
-            </List>
+          <Col gap="4rem">
+            <Section gap="2rem">
+              <h2 id="archive">Archive</h2>
+              <List nostyle gap="2rem">
+                {data.allMdx.nodes.map(post => {
+                  return <PostCard post={post} />
+                })}
+              </List>
+            </Section>
+            <Section gap="2rem">
+              <h2>Working Drafts</h2>
+              <List nostyle gap="2rem">
+                {data.draft.nodes.map(post => {
+                  return <PostCard post={post} />
+                })}
+              </List>
+              {/* <Label size="12px" transparent>
+                The behind-the-scenes. Embracing the work in progress.
+              </Label> */}
+            </Section>
           </Col>
         </Grid>
       </Section>
-      {/* <Section as="section" p="1rem 2rem">
-        <Grid>
-          <Col span="2">
-            <p>
-              This work journal is a work in progress. Do drop by once in a
-              while or consider following the project on{" "}
-              <AnimatedAnchor
-                href="//github.com/rickwsonlee/ricklee.co"
-                target="_blank"
-                rel="noopener"
-                underlined
-                external
-              >
-                <span>GitHub</span>
-              </AnimatedAnchor>
-              .
-            </p>
-          </Col>
-        </Grid>
-      </Section> */}
     </Layout>
   )
 }
@@ -186,19 +121,46 @@ export const siteQuery = graphql`
         description
       }
     }
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMdx(
+      filter: { frontmatter: { status: { eq: "Published" } } }
+      sort: { fields: frontmatter___date___published, order: DESC }
+    ) {
       nodes {
         fields {
           slug
         }
         frontmatter {
           title
+          subtitle
+          status
           featuredImage {
             src {
               childImageSharp {
                 gatsbyImageData(layout: FULL_WIDTH, aspectRatio: 1.33)
               }
             }
+          }
+          meta {
+            contribution
+            year
+          }
+        }
+      }
+    }
+    draft: allMdx(
+      filter: { frontmatter: { status: { eq: "Draft" } } }
+      sort: { fields: frontmatter___date___modified, order: DESC }
+    ) {
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          subtitle
+          status
+          date {
+            modified
           }
           meta {
             contribution

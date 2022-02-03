3f7338b9e89f671ade75205971640e18d094b507
Rick Lee <29583892+rickwsonlee@users.noreply.github.com>
2022-02-03 21:44:42 +0800

    Add underline to links on mobile

--- src/components/link.js
+++ src/components/link.js
@@ -8,18 +8,28 @@ const LinkAnimation = css`
     position: relative;
     margin-right: ${props => (props.external ? `.3em` : null)};
 
-    &:before {
-      content: "";
-      display: block;
-      width: 100%;
-      height: 1px;
-      position: absolute;
-      bottom: -1px;
-      left: 0;
-      background-color: ${({ theme }) => theme.text};
-      transform: ${props => (props.underlined ? `scaleX(1)` : `scaleX(0)`)};
-      transform-origin: top left;
-      transition: transform 0.25s ease-out;
+    ${props =>
+      props.underlined &&
+      css`
+        text-decoration: underline;
+      `}
+
+    @media (min-width: 43.75rem) {
+      text-decoration: none;
+
+      &:before {
+        content: "";
+        display: block;
+        width: 100%;
+        height: 1px;
+        position: absolute;
+        bottom: -1px;
+        left: 0;
+        background-color: ${({ theme }) => theme.text};
+        transform: ${props => (props.underlined ? `scaleX(1)` : `scaleX(0)`)};
+        transform-origin: top left;
+        transition: transform 0.25s ease-out;
+      }
     }
   }
 
@@ -27,33 +37,33 @@ const LinkAnimation = css`
     transform: ${props => (props.underlined ? `scaleX(0)` : `scaleX(1)`)};
   }
 
-  &::after {
+  /* &::after {
     ${props =>
-      props.external
-        ? css`
-            content: "";
-            display: inline-block;
-            width: 0.7em;
-            height: 0.7em;
-            -webkit-mask: url(${icon}) no-repeat 50% 50%;
-            mask: url(${icon}) no-repeat 50% 50%;
-            -webkit-mask-size: cover;
-            mask-size: cover;
-            background-color: ${({ theme }) => theme.text};
-            opacity: 0.5;
-            transition: opacity 0.25s ease-out;
-          `
-        : null}
+    props.external
+      ? css`
+          content: "";
+          display: inline-block;
+          width: 0.7em;
+          height: 0.7em;
+          -webkit-mask: url(${icon}) no-repeat 50% 50%;
+          mask: url(${icon}) no-repeat 50% 50%;
+          -webkit-mask-size: cover;
+          mask-size: cover;
+          background-color: ${({ theme }) => theme.text};
+          opacity: 0.5;
+          transition: opacity 0.25s ease-out;
+        `
+      : null}
   }
 
   &:hover::after {
     ${props =>
-      props.external
-        ? css`
-            opacity: 1;
-          `
-        : null}
-  }
+    props.external
+      ? css`
+          opacity: 1;
+        `
+      : null}
+  } */
 `
 
 export const AnimatedLink = styled(Link)`
